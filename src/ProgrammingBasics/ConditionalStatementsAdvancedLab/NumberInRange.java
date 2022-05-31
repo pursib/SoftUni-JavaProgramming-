77ad309832ee26d9170d7ed47809c42c01b717ca
Pursi <pursib@abv.bg>
2022-05-31 15:06:24 +0300

    Conditional Statements Advanced - Lab tasks FINISHED!

---
+++ src/ProgrammingBasics/ConditionalStatementsAdvancedLab/NumberInRange.java
@@ -0,0 +1,16 @@
+package ProgrammingBasics.ConditionalStatementsAdvancedLab;
+
+import java.util.Scanner;
+
+public class NumberInRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numb = Integer.parseInt(scanner.nextLine());
+
+        if(numb>= -100 && numb <=100 && numb !=0){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
